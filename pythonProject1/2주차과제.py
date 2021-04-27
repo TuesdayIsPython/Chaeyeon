f7e8d8df5e520a2b5aebf51fce10c32c07f94e49
Chaeyeon <lcy19980109@gmail.com>
2021-04-27 22:31:13 +0900

    2nd week homework

---
+++ pythonProject1/2주차과제.py
@@ -0,0 +1,14 @@
+'''
+TuesDayIsPython_0427
+'''
+
+
+#input
+a = input('이름을 입력해 주세요 : ')
+print('이름: 이채연')
+input('파트를 입력해 주세요 : ')
+print('소속파트: 디자인')
+input('어느 기수입니까? : ')
+print('기수: 28기')
+input('OB입니까 YB입니까?')
+print('저는: yb 입니다.')
